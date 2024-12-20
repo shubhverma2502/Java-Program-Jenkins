0b2d9baae2af38b156d44c6bd6bd5a2948fedab2
shubhverma2502 <117101649+shubhverma2502@users.noreply.github.com>
2024-12-20 10:37:13 +0530

    Initial commit

---
+++ Edvenswa.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Edvenswa{
+    public static void main(String[] args){
+        System.out.println("Jenkins Learning");
+        System.out.println(new Date());
+        System.out.println("Hello");
+
+    }
+}
